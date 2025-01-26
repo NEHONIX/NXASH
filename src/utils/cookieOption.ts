9cc4ff40c8317835a08a04b5ce7c71337b02ba7a
NEHONIX <sethndrieleazar@gmail.com>
2025-01-26 09:13:56 +0000

    Update cookieOption.ts

--- src/utils/cookieOption.ts
+++ src/utils/cookieOption.ts
@@ -4,10 +4,10 @@ const isDevelopment = process.env.NODE_ENV === "development";
 
 export const cookieOption: CookieOptions = {
   httpOnly: true,
-  secure: !isDevelopment,
-  sameSite: "none", //isDevelopment ? "lax" : 
+  secure: process.env.NODE_ENV === "production",
   path: "/",
-  maxAge: 30 * 24 * 60 * 60 * 1000, // 30 jours
+  sameSite: process.env.NODE_ENV === "production" ? "none" as const : "lax" as const,
+  maxAge: 3 * 24 * 60 * 60 * 1000, // 3 days
 };
 
 // Options pour la production
